62c1863e5a15c7f9a4bd3840cb2ae084078c5769
ArresT-AnarchY <arrest.anarchy@gmail.com>
2023-12-15 12:43:47 +0300

    Delete online map directory

--- online map/optmap/map.sidebar.opt.js
+++
@@ -1,4 +0,0 @@
-   //sidebar açılış
-   var sidebar = L.control.sidebar('sidebar').addTo(map);
-            //var sidebar = L.control.sidebar('sidebar', {position: 'right'}).addTo(map);
-   //sidebar.open('home');
